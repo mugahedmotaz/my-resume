27f002ee18a4e87715ae35e5185bfd75a658d294
Mugahed Motez <a0125544316@gmail.com>
2024-09-10 15:09:52 +0300

    new edit

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,18 +1,24 @@
-
 function emailSend() {
   Email.send({
-    SecureToken : "90416335-90b2-43b7-802b-ccd4ca7c9c70",
-    To : 'mugahedmotaz@gmail.com',
-    From :  'mugahedmotaz@gmail.com',
-    Subject : "This is the subject",
-    Body : "Email : " + document.getElementById("email").value+ "<br/> " +
-            "Subject : " + document.getElementById("subject").value+ "<br/> " +
-   "Message : " + document.getElementById("message").value+ "<br/> "
-    }).then( message => {
-        Swal.fire({
-          title:"تم إستلام رسالتك بنجاح",
-          text: "  سيتم الرد عليك في اقرب وقت    ",
-          icon: "success"
-        });
+    SecureToken: "90416335-90b2-43b7-802b-ccd4ca7c9c70",
+    To: "mugahedmotaz@gmail.com",
+    From: "mugahedmotaz@gmail.com",
+    Subject: "This is the subject",
+    Body:
+      "Email : " +
+      document.getElementById("email").value +
+      "<br/> " +
+      "Subject : " +
+      document.getElementById("subject").value +
+      "<br/> " +
+      "Message : " +
+      document.getElementById("message").value +
+      "<br/> ",
+  }).then((message) => {
+    Swal.fire({
+      title: "تم إستلام رسالتك بنجاح",
+      text: " سيتم الرد عليك في اقرب وقت",
+      icon: "success",
     });
-  }
+  });
+}
